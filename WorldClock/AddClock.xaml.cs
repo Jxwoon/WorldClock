b25e3c593a3eac1c659d7ef002612ea02d9b01ca
Jasmine Woon <Jasmine Woon@DESKTOP-J0BAQID>
2022-02-10 11:18:13 -0800

    Add project files.

---
+++ WorldClock/AddClock.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorldClock
+{
+    /// <summary>
+    /// Interaction logic for AddClock.xaml
+    /// </summary>
+    public partial class AddClock : UserControl
+    {
+        public AddClock()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTime(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+              string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+              "MainWindow");
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = new Button();
+            button.Content = "Foo";
+            //this.stackPanel1.Children.Add(button);
+            MessageBox.Show(
+              string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+              "MainWindow");
+        }
+    }
+}
---
+++ WorldClock/Clock.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WorldClock
+{
+    /// <summary>
+    /// Interaction logic for Clock.xaml
+    /// </summary>
+    public partial class Clock : UserControl
+    {
+       
+        public Clock()
+        {
+            InitializeComponent();
+           
+            StartClock();
+            
+        }
+        private void StartClock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickevent;
+            timer.Start();
+            setClockZone();
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            clockTime.Text = DateTime.Now.ToString();
+        }
+
+        private void setClockZone()
+        {
+            TimeZone localZone = TimeZone.CurrentTimeZone;
+            clockZone.Text = localZone.StandardName;
+        }
+
+        private void Delete_Clock(object sender, RoutedEventArgs e)
+        {
+            ((Panel)this.Parent).Children.Remove(this);
+        }
+    }
+}
